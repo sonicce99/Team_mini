497dbf7a2d44f018f093c5eaa8f99a346bd20413
imhaeunk <imhaeunk@gmail.com>
2021-11-11 01:54:15 +0900

    test

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <h1>Home page..</h1>
   <h2>{{ heropyMessage }}</h2>
+  <EditProduct/>
 </template>
 
 <script>
+import EditProduct from '~/components/EditProduct.vue'
 export default {
+  components: { EditProduct },
   computed: {
     heropyMessage() {
       return this.$store.state.heropy.message
