0db99df25d6bb6bef72ad2237cff985226f3fca1
iamidlek <iamidlek@naver.com>
2021-11-12 12:19:15 +0900

    영경님 계좌 추가 경로

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Home from './Home'
 import Certification from './Certification'
 import DomainAdmin from './DomainAdmin'
 import MyPage from './MyPage'
+import AddAccount from './AddAccount'
 import SignIn from '~/components/SignIn'
 import SignUp from '~/components/SignUp'
 import AllProducts from '~/components/AllProducts'
@@ -11,6 +12,7 @@ import EditProduct from '~/routes/EditProduct'
 import PurchaseList from '~/components/PurchaseList'
 import MyAccount from '~/components/MyAccount'
 import ChangeUserInfo from '~/components/ChangeUserInfo'
+
 
 export default createRouter({
   history: createWebHistory(),
@@ -56,6 +58,11 @@ export default createRouter({
           component: ChangeUserInfo
         }
       ]
+    },
+    {
+      name: 'AddAccount',
+      path: '/addaccount',
+      component: AddAccount
     },
     {
       path: '/admin',
