49137a750b8d83f11062960d32fd3781d4de063b
imparang <jeongjaeyn@gmail.com>
2021-11-12 16:13:11 +0900

    feat: Add confirm and cancel purchase api

--- src/utils/userApiConfig.js
+++ src/utils/userApiConfig.js
@@ -13,7 +13,27 @@ export const logOut = axios.create({
 })
 
 export const purchaseDetail = axios.create({
-  baseURL: 'ttps://asia-northeast3-heropy-api.cloudfunctions.net/api/products/transactions/details',
+  baseURL: 'ttps://asia-northeast3-heropy-api.cloudfunctions.net/api/products/ok',
+  headers: {
+    'content-type': 'application/json',
+    apikey: 'FcKdtJs202110',
+    username: "5zo",
+    authorization: `Bearer ${token}`
+  }
+})
+
+export const cancelPurchase = axios.create({
+  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/cancel',
+  headers: {
+    'content-type': 'application/json',
+    apikey: 'FcKdtJs202110',
+    username: "5zo",
+    authorization: `Bearer ${token}`
+  }
+})
+
+export const confirmPurchase = axios.create({
+  baseURL: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/ok',
   headers: {
     'content-type': 'application/json',
     apikey: 'FcKdtJs202110',
