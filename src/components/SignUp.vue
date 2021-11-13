e8ff2146b8e818d957f51d05594cd50b81d87e8b
dongsu <sonicce99@naver.com>
2021-11-13 20:42:06 +0900

    docs: 회원가입 페이지 완성

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,19 +1,26 @@
 <template>
-  <input 
-    v-model="id"
-    type="text" 
-    placeholder="아이디를 입력하세요 "/><br>
-  <input 
-    v-model="pw"
-    type="password" 
-    placeholder="비빌번호를 입력하세요 "/><br>
-  <input
- ㅎ   v-model="displayName"
-    type="text" 
-    placeholder="닉네임을 입력하세요 "/><br>  
-  <input type="file" @change="selectfile" /><br>  
-  <button @click="signUp">회원가입!</button>  
-
+  <form @submit.prevent="signUp">
+    <fieldset>
+      <legend>회원가입 정보 입력</legend>
+      <input 
+        v-model="email"
+        type="email" 
+        placeholder="이메일을 입력하세요 "
+        autofocus /><br>
+      <input 
+        v-model="pw"
+        type="password" 
+        minlength="8"
+        placeholder="비빌번호를 입력하세요 "
+        title="최소 8자리 이상 입력해주세요!"/><br>
+      <input
+        v-model="displayName"
+        type="text" 
+        placeholder="닉네임을 입력하세요 "/><br>  
+      <input type="file" @change="selectfile" /><br>  
+      <input type="submit" value="회원가입!" />
+    </fieldset>
+  </form>
   <RouterLink :to="{ name: 'SignIn' }">
     로그인으로
   </RouterLink>    
@@ -27,7 +34,7 @@ import { axiosAuthInit } from '~/utils/authenticationApiConfig'
 export default {
   data() {
     return {
-      id:"",
+      email:"",
       pw:"",
       displayName:"",
       profileImgBase64: null
@@ -47,16 +54,32 @@ export default {
       }
     },
     async signUp() {
-      const obj = {
-        email: this.id,
+      try {
+        const obj = {
+        email: this.email,
         password: this.pw,
         displayName: this.displayName,
         profileImgBase64: this.profileImgBase64
       }
       const data = await axiosAuthInit.post('signup', obj)
       console.log(data.data)
-      this.$router.push("/") //router로 메인페이지로 이동됨
+      // this.$router.push("/") //router로 메인페이지로 이동됨
+      } catch(error) {
+        console.log(error.response.data)
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+legend {
+  background-color: #000;
+  color: #fff;
+  padding: 3px 6px;
+}
+
+input {
+    margin: .2rem;
+}
+</style>
