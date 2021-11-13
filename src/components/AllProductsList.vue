5eaa8d7fe86df98f470ac1a487a0f9a64d482b63
imhaeunk <imhaeunk@gmail.com>
2021-11-13 20:31:36 +0900

    Test: Add RouterLink on AllProductsList for test - RequestPurchase's RouterLink will be on '개별제품 상세보기' component.
    -

--- src/components/AllProductsList.vue
+++ src/components/AllProductsList.vue
@@ -9,33 +9,18 @@
     <!-- 필수로 들어오는게 아닌 요소  -->
     <div>{{ product.tags.join(",") }}</div>
     <img :src="product.thumbnail" alt="">
-    <RouterLink :to="{ name: 'EditProduct', params: { id: `${product.id}` }}">수정하기</RouterLink>
+    <RouterLink :to="{ name: 'EditProduct', params: { id: product.id }}">수정하기</RouterLink>
     <!-- 구매신청은 상세페이지 안에 있어야함. 지금은 그냥 테스트용으로 여기에 만듦. -->
-    <button @click="toggleShow">구매하기</button>
-    <RequestPurchase v-if="show" :product="product"/>
+    <RouterLink :to="{ name: 'RequestPurchase', params: { id: product.id }, query: {title: product.title, price: product.price}}">구매하기</RouterLink>
   </div>  
 </template>
 
 <script>
-import RequestPurchase from './RequestPurchase'
 export default {
-   components: {
-      RequestPurchase
-  },
   props: {   
     product: {
       type: Object,
       default: () => ({})
-    }
-  },
-  data() {
-    return {
-      show: false,
-    }
-  },
-  methods: {
-    toggleShow() {
-      this.show = !this.show
     }
   }
 }
