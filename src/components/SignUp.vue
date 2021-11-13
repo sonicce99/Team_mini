73437a5619ef34cad706563cbf985314e2c5b298
dongsu <sonicce99@naver.com>
2021-11-13 17:53:46 +0900

    docs: 회원가입 페이지 생성

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>신규 회원 가입</div>
-
-  <input type="text" value="이메일 비번 등">
+  <input 
+    v-model="id"
+    type="text" 
+    placeholder="아이디를 입력하세요 "/><br>
+  <input 
+    v-model="pw"
+    type="password" 
+    placeholder="비빌번호를 입력하세요 "/><br>
+  <input
+    v-model="displayName"
+    type="text" 
+    placeholder="닉네임을 입력하세요 "/><br>  
+  <input type="file" @change="selectfile" /><br>  
+  <button @click="signUp">회원가입!</button>  
 
   <RouterLink :to="{ name: 'SignIn' }">
     로그인으로
-  </RouterLink>
+  </RouterLink>    
 </template>
+
+
+
+<script>
+import { axiosAuthInit } from '~/utils/authenticationApiConfig'
+
+export default {
+  data() {
+    return {
+      id:"",
+      pw:"",
+      displayName:"",
+      profileImgBase64: null
+    }
+  },
+  methods: {
+    selectfile(event) {
+      // console.log(event)
+      // const files= event.target.files
+      const { files }= event.target 
+      for(const file of files) {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.addEventListener("load" , () => {
+          this.profileImgBase64 = reader.result
+        })
+      }
+    },
+    async signUp() {
+      const obj = {
+        email: this.id,
+        password: this.pw,
+        displayName: this.displayName,
+        profileImgBase64: this.profileImgBase64
+      }
+      const data = await axiosAuthInit.post('signup', obj)
+      console.log(data.data)
+    }
+  }
+}
+</script>
