3c000611e51881c475fd26aa151502fdea3f4cb9
dongsu <sonicce99@naver.com>
2021-11-13 09:46:48 +0900

    docs: 단일 제품 상세 내역 모달창 만듬
    제품 상세 보기 버튼을 클릭하면 모달창을 띄워서 해당 제품에 대한 상세 내용을 표시함

--- src/components/SingleProductDetails.vue
+++ src/components/SingleProductDetails.vue
@@ -1,11 +1,26 @@
 <template>
   <button @click="SingleProductDetails">제품 상세 보기!</button>
+
+  <div class="black-bg" v-if="ModalOpened === true">
+    <div class="white-bg">
+      <h4>단일 제품 상세 조회 내역</h4>
+      <p>{{ SingleProductDetail }}</p>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      SingleProductDetail:{},
+      ModalOpened: false
+    }
+  },
   methods: {
     async SingleProductDetails() {
       try {
@@ -18,7 +33,9 @@ export default {
             "username": "5zo"
           }
         })
-        console.log(res.data)
+        console.log(res)
+        this.SingleProductDetail = res
+        this.ModalOpened = true
       } catch (error) {
         console.log(error.response.data)
       } 
@@ -26,3 +43,22 @@ export default {
   }  
 }
 </script>
+
+<style lang="scss" scoped>
+  div {
+    box-sizing: border-box;
+  }
+  .black-bg {
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    padding: 20px;
+  }
+  .white-bg {
+    width: 100%;  
+    padding: 20px;
+    background: white;
+    border-radius: 8px;      
+  }
+</style>
