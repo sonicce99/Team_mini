f9475d35d4039b4045e2bc1d07ac89010599258e
dongsu <sonicce99@naver.com>
2021-11-13 16:19:21 +0900

    docs: ul 태그 생성

--- src/components/AccountListChangeCheck.vue
+++ src/components/AccountListChangeCheck.vue
@@ -1,9 +1,11 @@
 <template>
   <h1>나의 계좌 리스트가 보여지고 있습니다!</h1>
-  <AccountList 
-  v-for="accountInfo in accountInfos" 
-  :key="accountInfo.id"
-  :accountInfo="accountInfo" />
+  <ul>
+    <AccountList 
+    v-for="accountInfo in accountInfos" 
+    :key="accountInfo.id"
+    :accountInfo="accountInfo" />
+  </ul>
 </template>
 
 <script>
