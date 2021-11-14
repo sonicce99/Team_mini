8c026e272d4fc5bf0df9feaf7ab60b70a5f7483a
imhaeunk <imhaeunk@gmail.com>
2021-11-14 22:29:18 +0900

    Fix: edit some tag's to comments
    -

--- src/components/PurchaseItem.vue
+++ src/components/PurchaseItem.vue
@@ -1,32 +1,44 @@
 <template>
 <!-- 최상위 RouterLink로 변경 가능 -->
-<div>
-  <!-- 단일 제품 상세 구매 내역 이동시 필요 -->
-  <div>{{ purchase.detailId }}</div>
-  <div>{{ purchase.timePaid }}</div>
-  <button @click="cancle(purchase.detailId)" :disabled="purchase.done">
-        구매취소
-  </button>
-  <div>{{ purchase.isCanceled }}</div>
-  <button @click="confirmed(purchase.detailId)">구매확인</button>
-  <div>{{ purchase.done }}</div>
-
-  <div>{{ purchase.product.productId }}</div>
-  <div>{{ purchase.product.title }}</div>
-  <div>{{ purchase.product.price }}</div>
-  <div>{{ purchase.product.description }}</div>
-  <div>{{ purchase.product.tags }}</div>
-  <img :src="purchase.product.thumbnail" alt="" />
-</div>
+  <ul class="purchased-item">
+    <!-- 단일 제품 상세 구매 내역 이동시 필요 -->
+    <!-- <li>{{ purchase.detailId }}</li> -->
+    <li>{{ purchase.timePaid }}</li>
+    <li>{{ purchase.product.title }}</li>
+    <li>{{ purchase.product.price }}</li>
+    <button @click="confirmed(purchase.detailId)">구매확인</button>
+    <button @click="cancle(purchase.detailId)" :disabled="purchase.done">
+          구매취소
+    </button>
+    <li>{{ purchase.done }}</li>
+    <li>{{ purchase.isCanceled }}</li>
+    <button @click="showPurchaseDetail">상세보기</button>
+    <!-- <li>{{ purchase.product.productId }}</li> -->
+    <!-- <li>{{ purchase.product.description }}</li>
+    <li>{{ purchase.product.tags }}</li> -->
+    <!-- <img :src="purchase.product.thumbnail" alt="" /> -->    
+  </ul>
+  <SingleProductDetailPurchaseList v-if="isShow" :detailId="purchase.detailId" />
+  <!-- 나중에 버튼으로 모달안에 상세구매내역 나타나게 할 것. -->
 </template>
 
 <script>
+import SingleProductDetailPurchaseList from '~/components/SingleProductDetailPurchaseList'
+
 export default {
+  data() {
+    return {
+      isShow: false,
+    }
+  },
   props: {
     purchase: {
       type: Object,
       default: () => ({})
     }
+  },
+  components: {
+    SingleProductDetailPurchaseList
   },
   methods: {
     async cancle(id) {
@@ -39,7 +51,19 @@ export default {
     },
     fetch() {
       console.log('구매 내역 불러오기')
-    }
+    },
+    showPurchaseDetail(){
+    this.isShow = !this.isShow
+    },
   }
 }
 </script>
+
+<style lang="scss" scoped>
+ .purchased-item {
+   list-style: none;
+   border: 2px solid red;
+   display: flex;
+   justify-content: space-around;
+ }
+</style>
