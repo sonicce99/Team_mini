b76686949fd7db9ef9fb8fad44968db1a2f62a99
imparang <jeongjaeyn@gmail.com>
2021-11-15 03:18:03 +0900

    feat: Apply style forms and buttons component

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,46 +1,43 @@
 <template>
-  <HomeHeader/>
-  <input
-    class="search"
-    type="text"
-    placeholder="상품을 검색하세요"
-  >
-    <div class="items">
-      검색된 상품이 표시됩니다
-    </div>
+  <HomeHeader />
+  <div class="input-group">
+    <i class="bx bx-search"></i>
+    <input class="form-input" type="text" placeholder="상품을 검색하세요" />
+  </div>
+  <div class="items">검색된 상품이 표시됩니다</div>
 </template>
 
 <script>
-import HomeHeader from "~/components/HomeHeader"
+import HomeHeader from '~/components/HomeHeader'
 
 export default {
   components: {
-    HomeHeader
+    HomeHeader,
   },
   methods: {
     logOut() {
-      sessionStorage.removeItem("token")
-      this.$store.dispatch("user/logOut")
-      this.$router.push("/")
-    }
-  }
+      sessionStorage.removeItem('token')
+      this.$store.dispatch('user/logOut')
+      this.$router.push('/')
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.search {
-  display: block;
-  margin: 50px auto;
-  text-align: center;
+.input-group {
+  width: 50%;
+  margin: 0 auto 60px;
+
+  .form-input {
+    padding-left: 40px;
+  }
 }
+
 .items {
   height: 500px;
   max-width: 700px;
   margin: 0 auto;
   border: 1px solid red;
-}
-
-input {
-  border: 1px solid #000;
 }
 </style>
