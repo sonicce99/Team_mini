107181a1bc86cb929bcdd6f9d03f3bd54eb91e7e
imparang <jeongjaeyn@gmail.com>
2021-11-15 14:56:56 +0900

    feat: Add to show search results logic and utilties funcitons

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -2,9 +2,39 @@
   <HomeHeader />
   <div class="input-group">
     <i class="bx bx-search"></i>
-    <input class="form-input" type="text" placeholder="상품을 검색하세요" />
+    <input
+      @keyup.enter="onKeyup(searchText)"
+      class="form-input"
+      type="text"
+      placeholder="상품을 검색하세요"
+      v-model="searchText"
+    />
+    <button
+      @click.prevent="resetQuery"
+      type="reset"
+      class="btn-reset"
+      v-if="isSearchInput"
+    >
+      <i class="bx bxs-x-circle"></i>
+    </button>
   </div>
-  <div class="items">검색된 상품이 표시됩니다</div>
+
+  <div v-if="isResult" class="items">
+    <ul>
+      <li v-for="searchResult in searchResults" :key="searchResult.id">
+        <h3>{{ searchResult.title }}</h3>
+        <strong>{{ searchResult.price }} </strong>
+        <p>{{ searchResult.description }}</p>
+        <span v-for="tag in searchResult.tags"> {{ tag }}</span>
+        <div>
+          <img
+            :src="`${searchResult.thumbnail}`"
+            :alt="`${searchResult.title}`"
+          />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -14,11 +44,34 @@ export default {
   components: {
     HomeHeader,
   },
+  data() {
+    return {
+      isSearchInput: false,
+      searchText: '',
+      tags: [],
+      isResult: false,
+    }
+  },
+  computed: {
+    searchResults() {
+      return this.$store.state.user.searchResults
+    },
+  },
   methods: {
     logOut() {
       sessionStorage.removeItem('token')
       this.$store.dispatch('user/logOut')
       this.$router.push('/')
+    },
+    async onKeyup(searchText) {
+      this.isSearchInput = true
+      this.isResult = true
+      await this.$store.dispatch('user/SHOW_SEARCHRESULTS', { searchText })
+    },
+    resetQuery() {
+      this.searchText = ''
+      this.isSearchInput = false
+      this.isResult = false
     },
   },
 }
@@ -26,11 +79,22 @@ export default {
 
 <style lang="scss" scoped>
 .input-group {
-  width: 50%;
+  width: 60%;
   margin: 0 auto 60px;
 
   .form-input {
     padding-left: 40px;
+  }
+
+  .btn-reset {
+    position: absolute;
+    top: 10px;
+    right: 12px;
+
+    .bxs-x-circle {
+      font-size: 22px;
+      color: $blue;
+    }
   }
 }
 
@@ -39,5 +103,14 @@ export default {
   max-width: 700px;
   margin: 0 auto;
   border: 1px solid red;
+}
+
+ul {
+  padding: 40px;
+
+  li {
+    border: 1px solid $border;
+    margin-bottom: 40px;
+  }
 }
 </style>
