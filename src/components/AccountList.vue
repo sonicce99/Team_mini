8c85c6eda6cc1df9fea8d24a749361867853c3c1
dongsu <sonicce99@naver.com>
2021-11-15 16:35:08 +0900

    변경사항 없음

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -3,7 +3,7 @@
     <!-- 사용자 페이지라서 아이디 숨김 -->
     <!-- <div>계좌 아이디: {{ accountInfo.id }}</div> -->
     <div>은행명: {{ accountInfo.bankName }}</div>
-    <div>은행 코드명: {{ accountInfo.bankCode }}</div>
+    <div>계은행 코드명: {{ accountInfo.bankCode }}</div>
     <div>계좌번호: {{ accountInfo.accountNumber }}</div>
     <div>현재 금액: {{ accountInfo.balance }}</div>
     <AccountRemove :id="accountInfo.id"/>
