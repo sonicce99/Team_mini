0c107339c9959501c6d5f19a072b02052ef718ed
dongsu <sonicce99@naver.com>
2021-11-15 16:19:01 +0900

    docs: 계좌 해지 완성 계좌 해지시 모달창에서 해지되었습니다 뜨고 닫기 버튼 클릭시 현재 사용가능한 계좌 리로드 됨

--- src/components/AccountRemove.vue
+++ src/components/AccountRemove.vue
@@ -3,6 +3,7 @@
 
   <div class="black-bg" v-if="removed === true">
     <div class="white-bg">
+      <button @click="closed" class="exit">닫기</button>
       <h4>해지되었습니다!</h4>
     </div>
   </div>
@@ -10,6 +11,7 @@
 
 <script>
 import { axiosAccount } from '~/utils/accountApiConfig'
+import router from '~/routes'
 
 export default {
   data() {
@@ -23,20 +25,23 @@ export default {
   methods: {
     async AccountRemove() {
       try {
-        // 추후에 Login Page에서 sessionStorage set 시켜야함. 현재 로그인 페이지가 없어서 작성을 못했음.
-        const token = sessionStorage.getItem("accessToken")
         const obj = {
           accountId: this.id,
           signature: true
         }
-        const { data } = await axiosAccount.delete('', obj)
-        console.log(data)        
+        const deleteApi = await axiosAccount.delete('',{data: obj})
+        console.log(deleteApi)
         this.removed = true
 
       } catch (error) {
         console.log(error.response.data)
-      } 
+      }
     },
+    // 닫기 버튼 클릭시 모달창 닫기
+    closed() {
+      this.removed = false
+      router.go()
+    }
   }  
 }
 </script>
@@ -57,5 +62,8 @@ export default {
     padding: 20px;
     background: white;
     border-radius: 8px;      
+  }
+  .exit {
+
   }
 </style>
