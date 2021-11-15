4003426127147458ff52acd9dd28c7cc4cbc722b
dongsu <sonicce99@naver.com>
2021-11-15 16:17:26 +0900

    docs: 사용자 페이지 계좌리스트에서 id 안보이게 함

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <div>계좌 아이디: {{ accountInfo.id }}</div>
+    <!-- 사용자 페이지라서 아이디 숨김 -->
+    <!-- <div>계좌 아이디: {{ accountInfo.id }}</div> -->
     <div>은행명: {{ accountInfo.bankName }}</div>
     <div>은행 코드명: {{ accountInfo.bankCode }}</div>
     <div>계좌번호: {{ accountInfo.accountNumber }}</div>
