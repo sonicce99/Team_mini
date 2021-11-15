2ebb887d3aab2c838623a1f1e88caf1a61ca8c12
iamidlek <iamidlek@naver.com>
2021-11-15 17:22:19 +0900

    feat: Filtering items => when select tag

--- src/components/AllProductsList.vue
+++ src/components/AllProductsList.vue
@@ -1,15 +1,14 @@
 <template>
-<!-- 최상위 RouterLink로 변경 가능 -->
-  <div>
-    <div>{{ product.id }}</div>
+  <div class="product">
+    <img :src="product.thumbnail? product.thumbnail: 'https://via.placeholder.com/150'" alt="">
     <div>{{ product.title }}</div>
-    <div>{{ product.price }}</div>
     <div>{{ product.description }}</div>
+    <div>{{ product.price }}</div>
     <div> {{ product.isSoldOut }}</div>
     <!-- 필수로 들어오는게 아닌 요소  -->
     <div>{{ product.tags.join(",") }}</div>
+    <div>{{ product.id }}</div>
 
-    <img :src="product.thumbnail? product.thumbnail: 'https://via.placeholder.com/150'" alt="">
     <RouterLink :to="{ name: 'EditProduct', params: { id: product.id }}">수정하기</RouterLink>
     <!-- 구매신청은 상세페이지 안에 있어야함. 지금은 그냥 테스트용으로 여기에 만듦. -->
     <RouterLink :to="{ name: 'RequestPurchase', params: { id: product.id }, query: {title: product.title, price: product.price}}">구매하기</RouterLink>
@@ -17,8 +16,18 @@
   </div>
 </template>
 
-<script>
+<style lang="scss" scoped>
+.product {
+  padding: 1rem;
+  display: flex;
+  border: 1px solid red;
+  img {
+    width: 90px;
+  }
+}
+</style>
 
+<script>
 export default {
   props: {   
     product: {
