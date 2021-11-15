4c5def0ce86e4c6c3d9effe6d1f9d9aeddcc4a2a
imparang <jeongjaeyn@gmail.com>
2021-11-16 00:37:44 +0900

    feat: Create ChangeUserInfo component

--- src/components/ChangeUserInfo.vue
+++ src/components/ChangeUserInfo.vue
@@ -1,5 +1,122 @@
 <template>
-  <div>
-    유저 정보 변경이 가능하다고 합니다
+  <div class="form-group">
+    <form class="input-group" @submit.prevent="updateInfo">
+      <h1>개인 정보 수정</h1>
+      <label for="user-name">이름</label>
+      <input
+        class="form-input"
+        v-model="displayName"
+        type="text"
+        id="user-name"
+        :placeholder="$route.query.name"
+      />
+      <label for="user-profile-picture">프로필 사진</label>
+      <input
+        class="form-input"
+        type="file"
+        id="user-profile-picture"
+        accept=".png, .jpg"
+        @change="selectfile"
+      />
+      <label for="old-password">기존 비밀번호</label>
+      <input
+        class="form-input"
+        v-model="oldPassword"
+        type="password"
+        id="old-password"
+        placeholder="기존 비밀번호을 입력하세요."
+        minlength="8"
+      />
+      <label for="new-password">새 비밀번호</label>
+      <input
+        class="form-input"
+        v-model="newPassword"
+        type="password"
+        id="new-password"
+        placeholder="새 비밀번호을 입력하세요."
+        minlength="8"
+      />
+
+      <input
+        @click="updateInfo"
+        class="btn-primary btn-16"
+        type="button"
+        value="정보 수정"
+      />
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      displayName: '',
+      oldPassword: '',
+      newPassword: '',
+      profileImgBase64: '',
+      data: {},
+    }
+  },
+  methods: {
+    async updateInfo() {
+      this.data = await this.$store.dispatch('user/UPDATE_USERINFO', {
+        displayName: this.displayName,
+        oldPassword: this.oldPassword,
+        newPassword: this.newPassword,
+        profileImgBase64: this.profileImgBase64,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.form-group {
+  @include column-flexbox();
+  width: 100%;
+
+  h1 {
+    @include text-style(32, $dark);
+    width: 360px;
+    font-weight: 300;
+    margin-bottom: 1em;
+  }
+
+  .input-group {
+    label {
+      @include text-style(12, $primary);
+      display: block;
+      width: 100%;
+    }
+
+    input {
+      display: block;
+      width: 360px;
+      padding-left: 12px;
+      margin-bottom: 12px;
+    }
+
+    input[type='file'] {
+      padding-top: 10px;
+      margin-bottom: 20px;
+      line-height: 1.1em;
+    }
+
+    input[type='button'] {
+      width: 360px;
+      margin-right: 20px;
+      cursor: pointer;
+    }
+  }
+
+  a {
+    @include flexbox();
+    position: absolute;
+    top: 40px;
+    left: 140px;
+    width: 80px;
+    margin-bottom: 12px;
+  }
+}
+</style>
