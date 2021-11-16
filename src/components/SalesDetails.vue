d14cd16ad57193e1ba17c0ed9fff2cd7f092f086
imparang <jeongjaeyn@gmail.com>
2021-11-16 14:42:17 +0900

    fix: Fix to store module name admin from user

--- src/components/SalesDetails.vue
+++ src/components/SalesDetails.vue
@@ -1,31 +1,25 @@
 <template>
   <h2>판매 내역</h2>
-    <ul>
-      <li v-for="item in salesDetails" :key="item">{{ item }}</li>
-    </ul>
+  <ul>
+    <li v-for="item in salesDetails" :key="item">{{ item }}</li>
+  </ul>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-
+import { axiosAdminProduct } from '~/utils/productApiConfig'
 export default {
-  // [vuex] unknown action type: FETCH 에러가 나옴 ㅜ
   created() {
     this.fetch()
   },
   computed: {
-    ...mapState('user',{
-      salesDetails: 'salesDetails'
-    })
+    salesDetails() {
+      return this.$store.state.admin.salesDetails
+    },
   },
   methods: {
-    ...mapActions('user', ['SHOW_SALESDETAILS']),
-    fetch() {
-      this.SHOW_SALESDETAILS()
+    async fetch() {
+      await this.$store.dispatch('admin/SHOW_SALESDETAILS')
     },
-    onAddProduct() {
-      this.$router.push('/admin')
-    }
-  }
+  },
 }
 </script>
