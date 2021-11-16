749d45955abbeb91f1dea34d8f0361581dc7d96a
imparang <jeongjaeyn@gmail.com>
2021-11-16 14:43:00 +0900

    fix: Remove useless router link

--- src/components/PurchaseList.vue
+++ src/components/PurchaseList.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>
-    <button @click="now = '구매'">구매 내역</button>
-    <button @click="now = '확정'">구매 확정 내역</button>
-    <button @click="now = '취소'">취소 내역</button>
-  </div>
-
   <div class="contents-box">
     <div v-if="!purchaseList.length">구매 신청 내역이 없습니다</div>
     <div v-else-if="isLoading">Loading...</div>
@@ -13,19 +7,22 @@
         <PurchaseItem
           v-for="purchase in purchaseList"
           :key="purchase.id"
-          :purchase="purchase" />
+          :purchase="purchase"
+        />
       </template>
       <template v-if="now === '확정'">
         <PurchaseItem
           v-for="purchase in doneList"
           :key="purchase.id"
-          :purchase="purchase" />
+          :purchase="purchase"
+        />
       </template>
       <template v-if="now === '취소'">
         <PurchaseItem
           v-for="purchase in canceledList"
           :key="purchase.id"
-          :purchase="purchase" />
+          :purchase="purchase"
+        />
       </template>
     </div>
   </div>
@@ -36,27 +33,27 @@ import PurchaseItem from '~/components/PurchaseItem'
 
 export default {
   components: {
-    PurchaseItem
+    PurchaseItem,
   },
   data() {
     return {
       isLoading: false,
-      now: '구매'
+      now: '구매',
     }
   },
   computed: {
     purchaseList() {
-      return this.$store.getters["user/purchaseOnlyRequested"]
+      return this.$store.getters['user/purchaseOnlyRequested']
     },
     doneList() {
-      return this.$store.getters["user/purchaseOnlyDone"]
+      return this.$store.getters['user/purchaseOnlyDone']
     },
     canceledList() {
-      return this.$store.getters["user/purchaseOnlyCanceled"]
-    }
+      return this.$store.getters['user/purchaseOnlyCanceled']
+    },
   },
   created() {
-    this.$store.dispatch("user/getPurchaseList")
-  }
+    this.$store.dispatch('user/getPurchaseList')
+  },
 }
 </script>
