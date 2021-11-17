6f0e322bd6ea2a3ead20c129218d5cf708098fec
imhaeunk <imhaeunk@gmail.com>
2021-11-17 12:46:44 +0900

    Design: edit accout design
    -

--- src/routes/RequestPurchase.vue
+++ src/routes/RequestPurchase.vue
@@ -1,36 +1,37 @@
 <template>
   <div class="request-area">
-    <h1>상품 구매</h1>
-      <table class="product-info" border="1">
-        <th>상품 정보</th>
-        <th>가격</th>
-        <tr>
-            <td>
-              <div class="product">
-                <img class="thumbnail" :src="$route.query.thumbnail" alt="제품 썸네일">
-                <span>{{ $route.query.title }}</span>
-              </div>
-            </td>
-            <td>
-            <div>{{ $route.query.price }}원</div>
-            </td>
-        </tr>
-    </table>    
-    <div class="account-info">
-      <div class="test"></div>
-      <h2>결제할 계좌 선택</h2>
-      <ul>
-        <li class="account" v-for="account in accounts" 
-          :key="account.id">           
-            <span>{{ account.bankName }}</span>
-            <strong>계좌번호</strong>
-            <span>{{ account.accountNumber }}</span>
-            <span>계좌 잔액: {{ account.balance }}</span>
-            <input type="radio" @click="setSelectedAccount" name="account" :value="account.id">
-        </li>
-      </ul>
+    <div class="content-wrap">
+      <h1>상품 구매</h1>
+        <table class="product-info" border="1">
+          <th>상품 정보</th>
+          <th>가격</th>
+          <tr>
+              <td>
+                <div class="product">
+                  <img class="thumbnail" :src="$route.query.thumbnail" alt="제품 썸네일">
+                  <span>{{ $route.query.title }}</span>
+                </div>
+              </td>
+              <td>
+              <div>{{ $route.query.price }}원</div>
+              </td>
+          </tr>
+      </table>    
+      <div class="account-info">
+        <h2>결제하실 계좌 선택</h2>
+        <ul>
+          <li class="account" v-for="account in accounts" 
+            :key="account.id">
+              <div class="bank-logo"></div>           
+              <span>{{ account.bankName }}</span>              
+              <span><strong>계좌번호</strong>{{ account.accountNumber }}</span>
+              <span>계좌 잔액: {{ account.balance }}</span>
+              <input class="select-radio" type="radio" @click="setSelectedAccount" name="account" :value="account.id">
+          </li>
+        </ul>
+      </div>
+      <button @click="requestPurchase">구매 신청</button>
     </div>
-    <button @click="requestPurchase">구매 신청</button>
   </div>  
 </template>
 
@@ -47,6 +48,7 @@ export default {
     }
   },
   mounted() {    
+    console.log(this.$route.query.thumbnail)
     this.loginCheck()
   },
   methods: {
@@ -94,51 +96,93 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .request-area{    
-    border: 5px solid #888;
-    padding: 20px;
-    margin: 0 auto;
+  .request-area{ 
+    max-width: 1200px;
+    margin: 0 auto; 
     text-align: center;
-    .product-info {
-      width: 80%;
-      h1 {
-      font-size: 35px;
-      font-weight: 800;
-      }
-    .product{
-      border: 1px solid red;
-      display: flex;
-      justify-content: space-evenly;
-      align-items: center;
-      .thumbnail {
-        width: 150px;
-        height: 150px;
-      }
-    }    
-  }
-  .account-info{
-    border: 1px solid blue;
-    width: 80%;  
-    ul{
-      display:flex;
-      justify-content: space-evenly;
-      align-items: center;
-      .account {
-        width: 300px;
-        height: 200px;
-        border: 1px solid lime;
-        background-image: url('https://gist.githubusercontent.com/iamidlek/978eb94e4e0c941b420a01269da9a824/raw/ab75c999255f5639e92354b2c222b482f3eaf662/bankName.png');
-        background-size: cover;
-        // background-color: #776c61;
-        span {
-          display:block;
-        }
-      }
+    background-color: $background; 
+    
+    h1 {
+      text-align: left;
+      margin: 50px 20px;
+      @include text-style(32, $dark)
     }
-    h2 {
-      font-size: 20px;
-      margin: 20px 0;
-    }    
-  }
+
+    .content-wrap{
+      padding: 20px;
+      width: 100%;
+      margin: 0 auto;
+
+      .product-info { 
+
+        th {
+          height: 44px;
+          padding: 4px 12px;
+          font-size: 16px;
+          vertical-align: middle;
+          background-color: #1f2d3d;
+          color: #fff;
+        }
+
+        td {
+          font-size: 15px;
+          font-weight: bold;
+        }
+
+        .product{
+          @include flexbox(around, center);          
+
+          .thumbnail {
+            width: 120px;
+            height: 110px;
+            margin-bottom: 5px;
+          }
+        }    
+      } 
+
+    .account-info {
+
+      h2{
+        @include text-style(24, $dark);
+        text-align: left;
+        margin: 40px 20px;
+      }
+
+      ul{
+        @include flexbox(around, center);
+        
+        .account {
+          @include flexbox(end, start);
+          flex-direction: column;         
+          width: 300px;
+          height: 180px; 
+          border: 1px solid lime;
+          background-color: #776c61;
+
+          .bank-logo{
+          background-image: url('https://gist.githubusercontent.com/iamidlek/978eb94e4e0c941b420a01269da9a824/raw/ab75c999255f5639e92354b2c222b482f3eaf662/bankName.png');
+                         
+          width: 207px;
+          height: 35px;
+          }
+
+          span {
+            flex-basis: 20%;
+          }
+          .select-radio{
+            margin: 5px auto;
+            width: 20px;
+            height:20px;
+          }
+
+          .kookmin{
+            background: url('https://gist.githubusercontent.com/iamidlek/978eb94e4e0c941b420a01269da9a824/raw/ab75c999255f5639e92354b2c222b482f3eaf662/bankName.png') no-repeat -30px -129px;
+	          width: 207px;
+	          height: 35px;
+          }
+        }
+      }  
+    }      
+  }   
 }
 </style>
