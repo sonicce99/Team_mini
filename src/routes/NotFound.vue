95d77429521847dadfe47dd63c87f61884e96c8c
iamidlek <iamidlek@naver.com>
2021-11-17 13:32:33 +0900

    feat: add NotFound.vue

---
+++ src/routes/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <div class="inner">
+      <h1>404 Page Not Found</h1>
+      <p>요청하신 페이지를 찾을 수 없습니다</p>
+      <div class="link">
+        <RouterLink class="btn btn-primary btn-16" to="/"> 홈으로</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .link {
+    text-align: center;
+    margin-top: 2rem;
+  }
+}
+</style>
